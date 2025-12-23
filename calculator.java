d243a1b1cc42068f9c6873175e16ae7f3426c863
Adithi-Yadav <bijjuadithiyadav@gmail.com>
2025-12-23 14:17:23 +0530

    Multiplication

--- calculator.java
+++ calculator.java
@@ -2,7 +2,7 @@ public class calculator{
     public static void main (String[]args)
     {
         int a=10,b=5;
-        System.out.println("Multiplication:"+ (a*b));
+        System.out.println("Multiplication :"+ (a*b));
 
     }
 }
